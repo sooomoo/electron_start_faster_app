4453a2da72cce093f02a49581534dcd3414e9c28
mur <mur@qq.com>
2025-04-29 09:51:00 +0800

    优化

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -21,6 +21,13 @@ import Versions from "./components/Versions.vue";
   <main class="main">
     <img alt="logo" class="logo" src="./assets/electron.svg" />
     <Versions />
+    <div>
+      实际上这篇分享的内容没有太多看起来非常硬核的技术手段，更多的是对当前性能瓶颈的测量，以及更聪明的「重新排列组合」，或者说采用了一系列使体验更好的策略，这对用户体验的提升是巨大的。这也给了
+      KAITIAN 很大的启发，基于这些，我们也重新审视并优化了 KAITIAN
+      的启动速度，使 KAITIAN
+      的启动由之前的接近3秒达到了500ms以内的速度，后面会分享一下基于这个分享的
+      KAITIAN 启动速度优化过程。
+    </div>
   </main>
 </template>
 
